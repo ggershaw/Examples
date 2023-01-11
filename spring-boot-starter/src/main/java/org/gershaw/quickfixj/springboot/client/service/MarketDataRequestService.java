17a5409319b172690811cc9740f49f9a30ebb291
Eduardo Sanchez-Ros <esanchezros@yahoo.es>
2023-01-11 12:41:15 +0100

    Using quickfix Application

--- spring-boot-starter/src/main/java/org/gershaw/quickfixj/springboot/client/service/MarketDataRequestService.java
+++ spring-boot-starter/src/main/java/org/gershaw/quickfixj/springboot/client/service/MarketDataRequestService.java
@@ -1,27 +1,32 @@
 package org.gershaw.quickfixj.springboot.client.service;
 
-import io.allune.quickfixj.spring.boot.starter.model.Logon;
 import io.allune.quickfixj.spring.boot.starter.template.QuickFixJTemplate;
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.gershaw.quickfixj.springboot.client.component.MarketDataRequestor;
 import org.gershaw.quickfixj.springboot.util.Utils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.event.EventListener;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import quickfix.Application;
+import quickfix.DoNotSend;
+import quickfix.FieldNotFound;
+import quickfix.IncorrectDataFormat;
+import quickfix.IncorrectTagValue;
 import quickfix.Initiator;
+import quickfix.Message;
+import quickfix.RejectLogon;
 import quickfix.SessionID;
-import quickfix.field.MDEntryType;
+import quickfix.UnsupportedMessageType;
 import quickfix.fix50sp2.MarketDataRequest;
 
 import java.util.Arrays;
 import java.util.Map;
 
 @ConditionalOnProperty(prefix = "quickfixj.client.requester", name = "request", havingValue = "true")
-@Service
+@Service("clientApplication")
 @Slf4j
-public class MarketDataRequestService {
+public class MarketDataRequestService implements Application {
     private final QuickFixJTemplate fixJTemplate;
     private final Character[] mdEntryTypes;
     private final Map<String, String> instruments;
@@ -30,16 +35,21 @@ public class MarketDataRequestService {
     public MarketDataRequestService(final QuickFixJTemplate fixJTemplate,
                                     @Value("${quickfixj.client.requester.mdentry}") final Character[] mdEntryTypes,
                                     @Value("#{${quickfixj.client.requester.instruments}}") final Map<String, String> instruments,
-                                    final Initiator initiator) {
+                                    @Lazy final Initiator initiator) {
         this.fixJTemplate = fixJTemplate;
         this.mdEntryTypes = Arrays.copyOf(mdEntryTypes, mdEntryTypes.length);
         this.instruments = Map.copyOf(instruments);
         this.initiator = initiator;
     }
 
-    @EventListener
-    public void handleOnLogon(final Logon logon) {
-        log.info("Received: {}", logon);
+    @Override
+    public void onCreate(SessionID sessionId) {
+        log.info("onCreate: {}", sessionId);
+    }
+
+    @Override
+    public void onLogon(SessionID sessionId) {
+        log.info("Received Logon: {}", sessionId);
         final MarketDataRequest marketDataRequest = MarketDataRequestor.of(mdEntryTypes, instruments);
         final Map<SessionID, Boolean>  failures = Utils.send(fixJTemplate, initiator, marketDataRequest);
         if(!failures.isEmpty()) {
@@ -47,4 +57,28 @@ public class MarketDataRequestService {
         }
     }
 
+    @Override
+    public void onLogout(SessionID sessionId) {
+        log.info("onLogout: {}", sessionId);
+    }
+
+    @Override
+    public void toAdmin(Message message, SessionID sessionId) {
+        log.info("toAdmin: {}", sessionId);
+    }
+
+    @Override
+    public void fromAdmin(Message message, SessionID sessionId) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon {
+        log.info("fromAdmin: {}", sessionId);
+    }
+
+    @Override
+    public void toApp(Message message, SessionID sessionId) throws DoNotSend {
+        log.info("toApp: {}", sessionId);
+    }
+
+    @Override
+    public void fromApp(Message message, SessionID sessionId) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
+        log.info("fromApp: {}", sessionId);
+    }
 }
--- spring-boot-starter/src/main/java/org/gershaw/quickfixj/springboot/server/service/MarketDataService.java
+++ spring-boot-starter/src/main/java/org/gershaw/quickfixj/springboot/server/service/MarketDataService.java
@@ -1,19 +1,6 @@
 package org.gershaw.quickfixj.springboot.server.service;
 
-import static org.gershaw.quickfixj.springboot.server.component.MarketDataIncrementalRefreshFactory.of;
-
-import io.allune.quickfixj.spring.boot.starter.model.FromApp;
 import io.allune.quickfixj.spring.boot.starter.template.QuickFixJTemplate;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.Executors;
-import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.Strings;
@@ -24,23 +11,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.event.EventListener;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import quickfix.Acceptor;
+import quickfix.Application;
+import quickfix.DoNotSend;
 import quickfix.FieldNotFound;
+import quickfix.IncorrectDataFormat;
+import quickfix.IncorrectTagValue;
 import quickfix.Message;
+import quickfix.RejectLogon;
 import quickfix.SessionID;
+import quickfix.UnsupportedMessageType;
 import quickfix.field.MsgSeqNum;
 import quickfix.field.MsgType;
 import quickfix.fix50sp2.MarketDataIncrementalRefresh;
 import quickfix.fix50sp2.MarketDataRequest;
 import quickfix.fix50sp2.component.InstrmtMDReqGrp;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
 @ConditionalOnProperty(prefix = "quickfixj.server.publisher", name = "publish", havingValue = "true")
-@Service
+@Service("serverApplication")
 @Slf4j
-public class MarketDataService {
+public class MarketDataService implements Application {
 
   private final QuickFixJTemplate quickFixJTemplate;
   private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
@@ -50,24 +54,13 @@ public class MarketDataService {
   private ScheduledFuture<?> scheduledFuture;
 
   public MarketDataService(final QuickFixJTemplate quickFixJTemplate,
-      final Acceptor acceptor,
+      @Lazy final Acceptor acceptor,
       @Value("${quickfixj.server.publisher.schedule.period}") final long period,
       @Value("${quickfixj.server.publisher.schedule.time-unit}") final TimeUnit timeUnit) {
     this.quickFixJTemplate = quickFixJTemplate;
     this.acceptor = acceptor;
     this.period = period;
     this.timeUnit = timeUnit;
-  }
-
-  @EventListener
-  public void handle(final FromApp fromApp) {
-    final Message message = fromApp.getMessage();
-    final String msgType = getMsgType(message);
-
-    if (msgType.equals(MsgType.MARKET_DATA_REQUEST)) {
-      final MarketDataRequest marketDataRequest = (MarketDataRequest) message;
-      schedule(marketDataRequest);
-    }
   }
 
   /**
@@ -101,6 +94,47 @@ public class MarketDataService {
       }
     }
     return msgType;
+  }
+
+  @Override
+  public void onCreate(SessionID sessionId) {
+    log.info("onCreate: {}", sessionId);
+  }
+
+  @Override
+  public void onLogon(SessionID sessionId) {
+    log.info("onLogon: {}", sessionId);
+  }
+
+  @Override
+  public void onLogout(SessionID sessionId) {
+    log.info("onLogout: {}", sessionId);
+  }
+
+  @Override
+  public void toAdmin(Message message, SessionID sessionId) {
+    log.info("toAdmin: {}", sessionId);
+  }
+
+  @Override
+  public void fromAdmin(Message message, SessionID sessionId) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon {
+    log.info("fromAdmin: {}", sessionId);
+  }
+
+  @Override
+  public void toApp(Message message, SessionID sessionId) throws DoNotSend {
+    log.info("toApp: {}", sessionId);
+  }
+
+  @Override
+  public void fromApp(Message message, SessionID sessionId) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
+    log.info("fromApp: {}", sessionId);
+    final String msgType = getMsgType(message);
+
+    if (msgType.equals(MsgType.MARKET_DATA_REQUEST)) {
+      final MarketDataRequest marketDataRequest = (MarketDataRequest) message;
+      schedule(marketDataRequest);
+    }
   }
 
   @RequiredArgsConstructor
